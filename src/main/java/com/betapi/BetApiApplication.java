02eccc3aa6721b9f2e2f710df7dd6086106c5438
Mehdi <mehdi.bakkali.94@gmail.com>
2019-08-09 18:30:24 +0200

    cors ok

--- src/main/java/com/betapi/BetApiApplication.java
+++ src/main/java/com/betapi/BetApiApplication.java
@@ -2,6 +2,15 @@ package com.betapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.Arrays;
+import java.util.Collections;
 
 @SpringBootApplication()
 public class BetApiApplication {
@@ -9,4 +18,16 @@ public class BetApiApplication {
 		SpringApplication.run(BetApiApplication.class, args);
 	}
 
+
+	@Bean
+	public CorsFilter corsFilter() {
+		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		final CorsConfiguration config = new CorsConfiguration();
+		config.setAllowCredentials(true);
+		config.setAllowedOrigins(Collections.singletonList("*"));
+		config.setAllowedHeaders(Arrays.asList("Origin", "Content-Type", "Accept"));
+		config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "OPTIONS", "DELETE", "PATCH"));
+		source.registerCorsConfiguration("/**", config);
+		return new CorsFilter(source);
+	}
 }
