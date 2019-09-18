e29810c2d084f2012ad69fc4b59768103607ae7f
Mehdi <mehdi.bakkali.94@gmail.com>
2019-09-18 12:45:15 +0200

    Actuator health disable details

--- deploy/start.sh
+++ deploy/start.sh
@@ -3,6 +3,6 @@ echo 'START - Building application with maven'
 cd '/home/ubuntu/bets-api'
 sudo mvn clean install -DskipTests
 echo 'START - Setting exe rights to bets-api.jar'
-sudo chmod 400 bets-api.jar
+sudo chmod 400 target/bets-api.jar
 echo 'START - Running application'
 sudo nohup java -jar target/bets-api.jar > /dev/null 2>&1 &
