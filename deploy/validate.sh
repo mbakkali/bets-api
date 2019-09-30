50727b79369e43b586943585a5f1595038dcec20
Mehdi <mehdi.bakkali.94@gmail.com>
2019-09-30 16:38:14 +0200

    New path for application.pid5

--- deploy/validate.sh
+++ deploy/validate.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 while [ true ]
 do
-    if [ "$(curl -s -k  http://localhost:8080/bets-api/actuator/health)" = '{"status":"UP"}' ]
+    if [ "$(curl -s -k  https://localhost:8080/bets-api/actuator/health)" = '{"status":"UP"}' ]
     then
         echo "VALIDATE - Server is up and running ! "
         exit 0
     else
-        echo "VALIDATE - Checking https://localhost:8080/actuator/health endpoint"
+        echo "VALIDATE - Checking https://localhost:8080/bets-api/actuator/health endpoint"
         sleep 3s
     fi
 done
