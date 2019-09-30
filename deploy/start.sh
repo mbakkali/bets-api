139d03c3e2e51fe7c0bfb8028bd5a69f5e0cbaeb
Mehdi <mehdi.bakkali.94@gmail.com>
2019-09-30 03:27:44 +0200

    Update appspec file

--- deploy/start.sh
+++ deploy/start.sh
@@ -1,8 +1,3 @@
 #!/usr/bin/env bash
-echo 'START - Building application with maven'
-cd '/home/ubuntu/bets-api'
-sudo mvn clean install -DskipTests
-echo 'START - Setting exe rights to bets-api.jar'
-sudo chmod 400 target/bets-api.jar
 echo 'START - Running application'
 sudo nohup java -jar target/bets-api.jar > /dev/null 2>&1 &
