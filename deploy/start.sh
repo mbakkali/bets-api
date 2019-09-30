b591bce8964e08b7467aa13f4d55b3459b2d4072
Mehdi <mehdi.bakkali.94@gmail.com>
2019-09-30 14:56:09 +0200

    New path for application.pid2

--- deploy/start.sh
+++ deploy/start.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 echo 'START - Setting exe rights to bets-api.jar'
-sudo chmod 400 target/bets-api.jar
-sudo chmod u+x deploy/start.sh
+sudo chmod u+x /home/ubuntu/bets-api/target/bets-api.jar
+sudo chmod u+x /home/ubuntu/bets-api/deploy/start.sh
+sudo chmod u+x /home/ubuntu/bets-api/deploy/stop.sh
+sudo chmod u+x /home/ubuntu/bets-api/deploy/before-install.sh
+sudo chmod u+x /home/ubuntu/bets-api/deploy/validate.sh
 echo 'START - Running application'
-sudo nohup java -jar target/bets-api.jar > /dev/null 2>&1 &
+sudo nohup java -jar /home/ubuntu/bets-api/target/bets-api.jar > /dev/null 2>&1 &
