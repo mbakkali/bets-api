46bfcd356af6d79abd073bc1e92f1a3414231459
Mehdi <mehdi.bakkali.94@gmail.com>
2019-10-08 00:53:12 +0200

    Mv jar in dest folder 2

--- deploy/start.sh
+++ deploy/start.sh
@@ -4,5 +4,5 @@ echo 'START - Setting exe rights to bets-api.jar'
 sudo mv /home/ubuntu/bets-api/target/bets-api.jar /home/ubuntu/bets-api/bets-api.jar
 sudo rm -rf /home/ubuntu/bets-api/target/
 sudo chmod 777 /home/ubuntu/bets-api/bets-api.jar
-echo 'START - Running application'
+echo 'START - Running application in background'
 sudo nohup java -jar /home/ubuntu/bets-api/bets-api.jar > /dev/null 2>&1 &
