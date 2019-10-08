e2c4570dc7ce37bd79d3b76f66bc2d35aea96f44
Mehdi <mehdi.bakkali.94@gmail.com>
2019-10-09 00:10:52 +0200

    Set root to folder files 3

--- deploy/stop.sh
+++ deploy/stop.sh
@@ -2,7 +2,8 @@
 # Script to stop the application
 PID_PATH="/home/ubuntu/bets-api/application.pid"
 if [ ! -f "$PID_PATH" ]; then
-   echo "STOP - Process Id FilePath($PID_PATH) Not found"
+   echo "STOP - Process Id FilePath($PID_PATH) Not found with application.pid : using pkill -9 <service-name>"
+   sudo pkill -9 -f bets-api
 else
 pid=`cat $PID_PATH`
        echo "STOP - Stopping app wih pid $pid"
