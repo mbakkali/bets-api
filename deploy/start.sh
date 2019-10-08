a478e1d92cf66bca53eb7e484978806c981e1822
Mehdi <mehdi.bakkali.94@gmail.com>
2019-10-08 23:51:32 +0200

    Set root to folder files

--- deploy/start.sh
+++ deploy/start.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 echo 'START - Setting exe rights to bets-api.jar'
-
 sudo mv /home/ubuntu/bets-api/target/bets-api.jar /home/ubuntu/bets-api/bets-api.jar
 sudo rm -rf /home/ubuntu/bets-api/target/
 sudo chmod 777 /home/ubuntu/bets-api/bets-api.jar
+sudo chmod 775 /home/ubuntu/bets-api
 echo 'START - Running application in background'
 sudo nohup java -jar /home/ubuntu/bets-api/bets-api.jar > /dev/null 2>&1 &
